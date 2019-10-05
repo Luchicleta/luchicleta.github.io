179949a3f33a872fdfc5bcf5b45802f8d9faf7bb
Tommaso Tocci <tommaso@tocci.pro>
2019-10-05 19:46:36 +0200

    Keep data only in json format

--- data/convert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < ${DIR}/ciclofficine.yaml > ${DIR}/ciclofficine.json
