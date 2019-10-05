afaf2de7451cb3f129e973208774983b37b6fdb1
Tommaso Tocci <tommaso@tocci.pro>
2019-10-05 21:09:09 +0200

    Add geo URIs
    Geo URI on addresses allow to launch a map intent on android systems

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ function create_clf_list_entry(clf) {
 	var clf_tile_template = document.querySelector('#clf_tile');
 	var clf_tile = document.importNode(clf_tile_template.content, true);
 	clf_tile.querySelector("p.title").textContent = clf.name;
-	clf_tile.querySelector("p.address_text").textContent = clf.position.address;
+	clf_tile.querySelector("a.address_text").textContent = clf.position.address;
+	clf_tile.querySelector("a.address_text").href =
+		'geo:' + clf.position.latitude + ',' + clf.position.longitude + '?q=' + clf.position.latitude + ',' + clf.position.longitude + '(' + clf.name + ')';
 	clf_tile.querySelector("a.url").href = clf.url;
 	clf_tile.querySelector("a.url").textContent = clf.url;
 	clf_tile.querySelector("p.working-days").textContent = clf["working days"];
