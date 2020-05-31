466963dd0682ad7760421d3be2f087b224fb3259
ael <tommy.ael@gmail.com>
2020-05-31 23:26:38 +0200

    Conditional URL

--- js/main.js
+++ js/main.js
@@ -5,8 +5,12 @@ function create_clf_list_entry(clf) {
 	clf_tile.querySelector("a.address_text").textContent = clf.position.address;
 	clf_tile.querySelector("a.address_text").href =
 		'geo:' + clf.position.latitude + ',' + clf.position.longitude + '?q=' + clf.position.latitude + ',' + clf.position.longitude + '(' + clf.name + ')';
-	clf_tile.querySelector("a.url").href = clf.url;
-	clf_tile.querySelector("a.url").textContent = clf.url;
+	if ('url' in clf) {
+		clf_tile.querySelector("a.url").href = clf.url;
+		clf_tile.querySelector("a.url").textContent = clf.url;
+	} else {
+		clf_tile.querySelector(".url-line").remove();
+	}
 	clf_tile.querySelector("p.working-days").textContent = clf["working days"];
 	return clf_tile;
 }
